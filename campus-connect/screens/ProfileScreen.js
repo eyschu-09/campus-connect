bcafe011a3d77fe5028d08a08f242a0d958998a2
EYSCHU BUDDI <buddieyschu4@gmail.com>
2026-07-31 10:58:13 +0530

    logout title added here

--- campus-connect/screens/ProfileScreen.js
+++ campus-connect/screens/ProfileScreen.js
@@ -9,7 +9,7 @@ export default function ProfileScreen({ navigation }) {
   return (
     <View>
       <Button
-        title="Logout"
+        title="Logout title"
         onPress={handleLogout}
       />
     </View>
